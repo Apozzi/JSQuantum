521e5c8429b153474377a4c996efc17e193bd1f5
Pozzi <eanderea1@gmail.com>
2021-06-14 01:02:32 -0300

    Anderson Pozzi - Firebase Configuration

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAl_RhVK-B42FyfJIisSjNwD51e4VsnsO8",
+    authDomain: "quantumjs-9e2ff.firebaseapp.com",
+    projectId: "quantumjs-9e2ff",
+    storageBucket: "quantumjs-9e2ff.appspot.com",
+    messagingSenderId: "1040130958370",
+    appId: "1:1040130958370:web:f0caf76eb2fbf13c5b967b"
+});
 
 ReactDOM.render(
   <React.StrictMode>
